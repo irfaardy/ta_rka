b083e225947b9a74b6744e226b93ffd1bcf49a1a
IRFA <irfardy2@gmail.com>
2020-07-15 14:08:52 +0700

    anggaran

--- application/controllers/MataAnggaran.php
+++ application/controllers/MataAnggaran.php
@@ -6,7 +6,7 @@ class MataAnggaran extends CI_Controller {
 	function __construct(){
 		parent::__construct();
 
-		$this->auth->protect(4);
+		$this->auth->protect([4,5]);
 
 	}
 	public function index() {

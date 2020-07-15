3787c1faee2431855cd0d668dfa309fef8eb797e
2nd_EA <ahmad_ramli@outlook.com>
2020-07-15 19:51:18 +0700

    edit dashboard page

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -1,3 +1,13 @@
 <div class="container">
-	Hai, <?= AuthData()->nama ?> <a href="<?= base_url('/logout?token='.$this->session->login_token) ?>" class="btn btn-success">Logout</a>  <a href="<?= base_url('/users') ?>" class="btn btn-success">Kelola User</a>
+	<div class="row">
+		<div class="col">
+			<h4>Selamat Datang <b><?= AuthData()->nama ?></b></h4>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+		</div>
+	</div>
 </div>

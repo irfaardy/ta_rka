5c232878c4a23eb8e4d2665efea75402fd086062
IRFA <irfardy2@gmail.com>
2020-07-19 08:03:02 +0700

    ADD Favicon

--- application/views/layouts/header.php
+++ application/views/layouts/header.php
@@ -3,6 +3,7 @@
 <head>
 	<title><?= !empty($title) ? $title:"Dashboard" ?> | Sistem Informasi Rencana Kerja dan Anggaran Jurusan Universitas Jendral Achmad Yani </title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" href="<?= assets('img/logo-unjani.png') ?>">
 	<!-- CSS -->
 	<?php include('assets/css.php') ?>
 	<!-- ENDCSS -->

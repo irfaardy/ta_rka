1842c2e4070e02162967d0998061632aa639826a
IRFA <irfardy2@gmail.com>
2020-07-22 20:20:10 +0700

    Tambah Form Cetak Data

--- application/views/fasilitas/index.php
+++ application/views/fasilitas/index.php
@@ -1,6 +1,11 @@
 <div class="container-fluid">
   <div class="row">
     <div class="col">
+    	<?php if(AuthData()->level == 4): ?>
+    		<!-- Cetak Data -->
+    	 <?php $this->view('fasilitas/_cetak', array('url' => base_url('/Fasilitas/cetak'))); ?>
+    	
+    	<?php endif; ?>
       <a href="<?php echo base_url('/Fasilitas/tambah') ?>" class="btn btn-primary btn-sm float-right d-block"><i class="fas fa-plus fa-fw"></i> Tambah</a>
 
       <!-- load table -->

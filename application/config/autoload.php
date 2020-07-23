d33d37821850707f1c7de39d57927e021badb3a6
IRFA <irfardy2@gmail.com>
2020-07-23 09:25:45 +0700

    DomPDF Plugin

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database', 'session', 'auth', 'user_agent','form_validation');
+$autoload['libraries'] = array('database', 'session', 'auth', 'user_agent','form_validation','pdf');
 
 /*
 | -------------------------------------------------------------------
